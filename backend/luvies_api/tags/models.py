2a59665f00ed88f3337939a7409ec173e55b8547
Luisss003 <luis.saenz.compsci@gmail.com>
2025-06-02 21:44:08 -0500

    Added created_at field.

--- backend/luvies_api/tags/models.py
+++ backend/luvies_api/tags/models.py
@@ -7,6 +7,7 @@ class Tag(models.Model):
     movie = models.ForeignKey(Movie, related_name='tags', on_delete=models.CASCADE)
     tag_text = models.CharField(max_length=50)
     created_by = models.ForeignKey(User, related_name='tags', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
