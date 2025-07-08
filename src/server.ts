11bacb7865890308027527e5823c655d5df48348
Luisss003 <luis.saenz.compsci@gmail.com>
2025-07-07 23:39:48 -0500

    Created MySQL connection as well as running server listener.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
 import dotenv from 'dotenv';
 import app from './app';
-import mongoose from 'mongoose';
+import { Sequelize } from 'sequelize';
+
 dotenv.config({path: './../config.env'});
 
-//Create server
-const server = app.listen(process.env.SERVER_PORT, () => {
-    console.log('server has started...');
-});
+//Create Sequelize instance
+export const sequelize = new Sequelize('Luvies', process.env.MY_SQL_USERNAME!, 
+                                process.env.MY_SQL_PASSWRD!, {
+                                    host: process.env.MY_SQL_HOST!,
+                                    dialect: 'mysql',
+                                });
+
+//Connect to MySQL remote server, create tables, then start server
+(async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Connection has been established to Luvies...');
+        //Create tables in Luvies DB
+
+
+        //Begin server listener
+        app.listen(8000, () => {
+            console.log('Express server has started...');
+        });
+    }catch(error){
+        throw new Error('Unable to connect to database...');
+    }
+})
