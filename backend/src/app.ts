bf61ca85a8982a8dbba170f21e177cc6de26d0fb
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-09-16 00:04:32 -0500

    Added CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 let app = express();
+import cors from "cors";
 import moviesRouter from './routes/moviesRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import tagsRouter from './routes/tagsRoutes.js';
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/movies', moviesRouter)
